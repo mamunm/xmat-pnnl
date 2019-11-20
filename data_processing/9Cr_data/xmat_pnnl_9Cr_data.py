df24d97eb8c1fc16b4383b8f7ca6293f6c030c3c
mamu867 <mdosman.mamun@pnnl.gov>
2019-11-20 10:56:29 -0800

    master curve stuffs added

--- data_processing/9Cr_data/xmat_pnnl_9Cr_data.py
+++ data_processing/9Cr_data/xmat_pnnl_9Cr_data.py
@@ -5,10 +5,16 @@
 import numpy as np
 import pandas as pd
 import xmat_pnnl_code as xcode
+import mendeleev
 
 #Load the data
 data_df, features_description, alloy_metadata = xcode.load_data('9Cr_Data')
 
+ele = [k for k, v in features_description.items() if 'Element' in v]
+AN = {k: getattr(mendeleev, k).atomic_number for k in ele}
+data_df['Weighted_AN'] = 0
+for e in ele:
+    data_df['Weighted_AN'] += data_df[e].fillna(0) * AN[e] / 100
 #Save the cleaned data into a csv for future reference
 data_df.to_csv('Cleaned_data.csv', index=False)
 np.save('features.npy', features_description)
